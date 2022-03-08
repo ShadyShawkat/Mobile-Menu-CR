e6694d52cf4bed1c87ac42f64d4b012d6d851096
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-08 17:17:04 +0200

    Add mobile menu

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM fully loaded and parsed');
+  const body = document.querySelector('body');
+  const hamMenu = document.querySelector('.menu-bars');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const mobileMenuOptions = document.querySelector('.mobile-menu-options');
+  const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
+
+  const mobileMenuToggle = () => {
+    mobileMenu.classList.toggle('d-flex');
+    body.classList.toggle('overflow-hidden');
+  }
+
+  [hamMenu, mobileMenuCloseBtn, ...mobileMenuOptions.children].forEach(el => {
+    el.addEventListener('click', mobileMenuToggle)
+  })
 });
